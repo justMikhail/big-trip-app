36658741553142dd9f05f01da16039c9fc20665d
Mikhail Sitnikov <mikhail.walle@gmail.com>
2021-07-28 05:13:26 +0300

    add "createTripInfoTemplate"

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import {createSiteMenuTemplate} from './view/createSiteMenuTemplate';
+import {createTripInfoTemplate} from './view/createTripInfoTemplate';
 
 const render = (container, template, where) => {
   container.insertAdjacentHTML(where, template);
 };
 
 const headerTripMain = document.querySelector('.trip-main');
-
 const headerMenuContainer = headerTripMain.querySelector('.trip-controls__navigation');
 
 render(headerMenuContainer, createSiteMenuTemplate(), 'beforeEnd');
+render(headerTripMain, createTripInfoTemplate(), 'afterBegin');
 
