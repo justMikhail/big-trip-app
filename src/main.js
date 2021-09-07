aa84a2971c118727fceecc2db18a05c8632a542a
Mikhail Sitnikov <mikhail.walle@gmail.com>
2021-09-08 02:50:02 +0300

    Update "main.js" (use "TripEventsPresenter" fo render).

--- src/main.js
+++ src/main.js
@@ -1,15 +1,11 @@
 import TripInfoView from './view/trip-info';
 import TripControlsView from './view/trip-controls';
 import EventsFilterView from './view/events-filter';
-import EventsSortView from './view/events-sort';
-import EventsListView from './view/events-list';
-import EventItemView from './view/event-item';
-import EventItemFormView from './view/event-item-form';
-import EmptyEventsListView from './view/empty-events-list';
 
-import {render, RenderPosition, replace} from './utils/render';
+import {render, RenderPosition} from './utils/render';
 
 import {mockEventItems} from './mock/mock-event-data';
+import TripEventsPresenter from './presenter/trip-events-presenter';
 
 const eventItems = mockEventItems;
 
@@ -26,56 +22,7 @@ const renderUI = () => {
   render(eventsFilterContainer, new EventsFilterView(), RenderPosition.BEFORE_END);
 };
 
-const renderEvent = (container, event) => {
-  const eventComponent = new EventItemView(event);
-  const eventEditComponent = new EventItemFormView(event);
-
-  const replaceItemToItemForm = () => {
-    replace(eventEditComponent, eventComponent);
-  };
-
-  const replaceItemFormToItem = () => {
-    replace(eventComponent, eventEditComponent);
-  };
-
-  const onEscKeyDown = (evt) => {
-    if (evt.key === 'Escape' || evt.key === 'Esc') {
-      evt.preventDefault();
-      replaceItemFormToItem();
-      document.removeEventListener('keydown', onEscKeyDown);
-    }
-  };
-
-  eventComponent
-    .setEditClickHandler(() => {
-      replaceItemToItemForm();
-      document.addEventListener('keydown', onEscKeyDown);
-    });
-
-  eventEditComponent
-    .setFormSubmitHandler(() => {
-      replaceItemFormToItem();
-      document.removeEventListener('keydown', onEscKeyDown);
-    });
-
-  render(container, eventComponent, RenderPosition.BEFORE_END);
-};
-
-const renderTripEvents = (container, eventsList) => {
-  const eventsListComponent = new EventsListView();
-  const emptyEventsListComponent = new EmptyEventsListView();
-
-  render(container, eventsListComponent, RenderPosition.BEFORE_END);
-
-  if (!eventItems.length) {
-    render(eventsListComponent, emptyEventsListComponent, RenderPosition.BEFORE_END);
-    return;
-  }
-
-  render(container, new EventsSortView(), RenderPosition.AFTER_BEGIN);
-
-  eventsList.forEach((event) => renderEvent(eventsListComponent, event));
-};
+const tripEventsPresenter = new TripEventsPresenter(eventsContainer);
 
 renderUI();
-renderTripEvents(eventsContainer, eventItems);
+tripEventsPresenter.init(eventItems);
