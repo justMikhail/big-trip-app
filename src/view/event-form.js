76f52f5e4d9246a3fb9d4ae7a8bf4e6bfb535e5b
Mikhail Sitnikov <mikhail.walle@gmail.com>
2021-09-18 20:34:22 +0300

    "hide event-form" btn fix

--- src/view/event-form.js
+++ src/view/event-form.js
@@ -188,7 +188,10 @@ const createEventFormTemplate = (event) => {
         </div>
 
         <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
-        <button class="event__reset-btn" type="reset">Cancel</button>
+        <button class="event__reset-btn" type="reset">Delete</button>
+        <button class="event__rollup-btn" type="button">
+          <span class="visually-hidden">Open event</span>
+        </button>
       </header>
       <section class="event__details">
         ${offersForCurrentEventType}
