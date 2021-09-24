f54a9a9d1bec4982992ccd6bab5f57500fe62a48
Mikhail Sitnikov <mikhail.walle@gmail.com>
2021-09-25 02:49:50 +0300

    Refactoring(add new const for API)

---
+++ src/const/api-const.js
@@ -0,0 +1,12 @@
+export const Method = {
+  GET: 'GET',
+  PUT: 'PUT',
+  POST: 'POST',
+  DELETE: 'DELETE',
+};
+
+export const SourceURL = {
+  POINTS: 'points',
+  OFFERS: 'offers',
+  DESTINATIONS: 'destinations',
+};
